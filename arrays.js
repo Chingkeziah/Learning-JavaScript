eeb51516af4fca9307b42f857cb7a625e69105f3
Chingkeziah <chingkeziah@gmail.com>
2024-05-12 23:18:43 +0100

    Learning about array methods

--- arrays.js
+++ arrays.js
@@ -1,3 +1,28 @@
 // An Array literal
 const colors = ["Red", "Green", "Blue"];
 console.log(colors)
+
+// array methods
+// pop()
+const alphabets = ["a", "b", "c", "d", "e", "f"];
+alphabets.pop()
+
+// push()
+alphabets.push("g");
+console.log(alphabets)
+
+// shift
+alphabets.shift();
+console.log(alphabets)
+
+// unshift
+alphabets.unshift("g");
+console.log(alphabets)
+
+// splice()
+alphabets.splice(1, 0, "g", "h", "i")
+console.log(alphabets)
+
+// slice()
+alphabets.slice(1, 4);
+console.log(alphabets)
