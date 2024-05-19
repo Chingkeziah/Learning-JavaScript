c8c06fd540420a07caaecad1b85e08f1ba0d07f6
Chingkeziah <chingkeziah@gmail.com>
2024-05-19 22:10:59 +0100

    An example to demonstrate

--- Day 14/do-while-loops.js
+++ Day 14/do-while-loops.js
@@ -4,3 +4,11 @@ do {
 } 
   while (i<=5);
 
+// demonstrating how the do while loop works
+// that is, it executes a block of code before checking it the condition is true
+// in this case it isn't, therefore it'll execute the code once and stop.
+let age = 21;
+do {
+  console.log(age);
+}
+  while(age<10);
