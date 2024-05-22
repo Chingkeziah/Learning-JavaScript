05ddca08311ced396cd6eb0ed70a4fd7fbd55b06
Chingkeziah <chingkeziah@gmail.com>
2024-05-22 21:53:15 +0100

    Fixed typo

--- Day 17/exercise1.js
+++ Day 17/exercise1.js
@@ -6,9 +6,9 @@
 
 for(i=0; i<16; i++){
     if(i % 2 === 0){
-        console.log(i + " " + " is even");
+        console.log(i + " " + "is even");
     }
     else{
-        console.log(i + " " + " is odd");
+        console.log(i + " " + "is odd");
     }
 }
