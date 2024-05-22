d4c9b6ea238ce70a2ca71bac0a406d52be69ac4b
Chingkeziah <chingkeziah@gmail.com>
2024-05-22 22:11:40 +0100

    Exercise 2 still pending completion

---
+++ Day 17/exercise2.js
@@ -0,0 +1,35 @@
+// Write a JavaScript program that computes the average marks of the following students. 
+// Then, this average is used to determine the corresponding grade.
+const report = [
+    {
+        id: 1,
+        name: "David",
+        score: 80,
+    },
+    {
+        id: 2,
+        name: "Vinoth",
+        score: 77,
+    },
+    {
+        id: 3,
+        name: "Divya",
+        score: 88,
+    },
+    {
+        id: 4,
+        name: "Ishitha",
+        score: 95,
+    },
+    {
+        id: 5,
+        name: "Thomas",
+        score: 68,
+    }
+]
+// if(report.score <60){
+//     console.log("Grade F");
+// }
+// else(report.scpre <70){
+//     console.log("Grade D");
+// }
