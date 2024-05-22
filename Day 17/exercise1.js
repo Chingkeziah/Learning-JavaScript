488bce0342ff80d34ec3e4b3f8323abda4b76b04
Chingkeziah <chingkeziah@gmail.com>
2024-05-22 21:52:40 +0100

    Exercise 1 complete

--- Day 17/exercise1.js
+++ Day 17/exercise1.js
@@ -1,9 +1,14 @@
+//Write a JavaScript for loop that iterates from 0 to 15. For each iteration, it checks if the current number is odd or even, and displays a message on the screen.
+//Sample Output :
+//"0 is even"
+//"1 is odd"
+//"2 is even"
 
 for(i=0; i<16; i++){
     if(i % 2 === 0){
-        console.log(i + " " + "This number is even");
+        console.log(i + " " + " is even");
     }
     else{
-        console.log(i + " " + "This number is odd");
+        console.log(i + " " + " is odd");
     }
 }
