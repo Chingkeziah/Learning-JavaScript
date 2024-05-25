c9cbacfcc504c88644b2d84d772b884811fa59f4
Chingkeziah <chingkeziah@gmail.com>
2024-05-25 23:30:48 +0100

    Illustrated an example of the else if statement

---
+++ Day 20/else-if.js
@@ -0,0 +1,17 @@
+let age = 25;
+
+if (age < 13) {
+    console.log("You are not eligible to create an account");
+} else if (age >= 13 && age < 21) {
+    console.log("You are partly eligible, you can create account with the help of a guardian");
+} else {
+    console.log("you are eligible")
+}
+let number = 24;
+if (age < 13) {
+    console.log("You are not eligible to create an account");
+} else if (age >= 13 && age < 21) {
+    console.log("You are partly eligible, you can create account with the help of a guardian");
+} else {
+    console.log("you are eligible")
+}
