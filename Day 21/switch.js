339a4c981b589f7972788daa8100fad0b2afd9fd
Chingkeziah <chingkeziah@gmail.com>
2024-05-26 22:50:33 +0100

    Day 21: the switch statement

---
+++ Day 21/switch.js
@@ -0,0 +1,34 @@
+// syntax to get day
+let day = new Date().getDay();
+// leaving dayName undefined or without a value because we want the switch statement
+// to determine the appropriate value for it
+let dayName;
+
+switch(day) {
+    case 0:
+        dayName = "Sunday";
+        break;
+    case 1:
+        dayName = "Monday";
+        break;
+    case 2:
+        dayName = "Tuesday";
+        break;
+    case 3:
+        dayName = "Wednesday";
+        break;
+    case 4:
+        dayName = "Thursday";
+        break;
+    case 5:
+        dayName = "Friday";
+        break;
+    case 6:
+        dayName = "Saturday";
+        break;
+    default:
+        dayName = "Invalid day";
+}
+
+console.log("Today is " + dayName);
+
