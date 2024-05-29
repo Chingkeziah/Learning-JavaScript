3208e122e2bf52ebd66ce372e20354a0c7a9909f
Chingkeziah <chingkeziah@gmail.com>
2024-05-29 17:41:19 +0100

    Math properties

---
+++ Day 23/mathproperties.js
@@ -0,0 +1,8 @@
+// Math.E — Euler’s number, the base of natural logarithms, approximately 2.718
+// Math.LN10 — The natural logarithm of 10, approximately 2.302
+// Math.LN2 — The natural logarithm of 2, approximately 0.693
+// Math.LOG10E - The base 10 logarithm of E, approximately 0.434
+// Math.LOG2E - The base 2 logarithm of E, approximately 1.442
+// Math.PI - The ratio of the circumference of a circle to its diameter, approximately 3.14159
+// Math.SQRT1_2 - The square root of 1/2, approximately 0.707
+// Math.SQRT2 - The square root of 2, approximately 1.414
