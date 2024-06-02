5ac00e909c02531449693aa4e109eae5cb19aa29
Chingkeziah <chingkeziah@gmail.com>
2024-06-02 22:19:00 +0100

    Day 28: Completing the Math methods

---
+++ Day 28/math.pow().js
@@ -0,0 +1,3 @@
+// The ma.pow() method allows you to calculate the based of a number raised to a specific power
+// In this method, the first figure is usually the base while the second is the numerator
+Math.pow(base, numerator)
