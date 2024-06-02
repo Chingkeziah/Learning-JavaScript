c71ed32f0df9b93aa392b6129ed9259318e7db55
Chingkeziah <chingkeziah@gmail.com>
2024-06-02 23:09:10 +0100

    Completed the Math series

--- Day 28/Math.random.js
+++ Day 28/Math.random.js
@@ -1,3 +1,3 @@
-// The Math.random() method generates a random number between 0 and 1 with 1 and 0 being inclusive
-// It takes the syntax
+// The Math.random() method generates a random number between
+//  0 and 1 with 0 being inclusive and 1 being exclusive.
 Math.random()
--- Day 28/math.pow().js
+++ Day 28/math.pow().js
@@ -1,6 +1,8 @@
-// The ma.pow() method allows you to calculate the based of a number raised to a specific power
-// In this method, the first figure is usually the base while the second is the numerator
-Math.pow(base, numerator)
+// The math.pow() method takes two arguments which we can refer too as x and y. 
+// This method calculates the value of x to the power y and returns the answer. 
+// x is usually the first argument and y is the power.
+// syntax
+Math.pow(x, y)
 
 // For example;
 Math.pow(8, 2)
